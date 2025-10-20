7123bc7b379496bc3fbec41d76f2b375256a2dd8
shubtheone <impeccableshub@gmail.com>
2025-10-20 12:28:21 +0530

    b

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from vercel_wsgi import handle
 from app import app as flask_app
 
-def handler(environ, start_response):
-    return handle(flask_app, environ, start_response)
+def handler(event, context):
+    return handle(flask_app, event, context)
